260e009cd4a6987c5f61955e90bcfa93cf7bdc37
Panth <ptd5241t@gmail.com>
2025-07-12 21:35:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (58.33%) - LeetHub

---
+++ 3625-stone-removal-game/3625-stone-removal-game.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def canAliceWin(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if 9<n<19 or 26<n<34 or 39<n<45 or 48<n:
+            return True
+        else:
+            return False
