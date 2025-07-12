d335392876477a60af794f05c7d632ee8a0f227c
Panth <ptd5241t@gmail.com>
2025-07-12 21:14:09 +0530

    Time: 0 ms (100%), Space: 13.3 MB (92.45%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        loc=-1
+        b=0
+        e=len(nums)-1
+        while b<=e:
+            mid=int((b+e)/2)
+            if nums[mid]==target:
+                return mid
+            elif target<nums[mid]:
+                e=mid-1
+            else:
+                b=mid+1
+        return loc
